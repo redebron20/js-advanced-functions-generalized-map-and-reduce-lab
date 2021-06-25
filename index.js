76926da99ff9e3795e3684912a69d5fc4a96f2de
redebron20 <redebron20@gmail.com>
2021-06-25 09:23:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your functions here
+function map(src, callback) {
+    let r = [];
+
+    for (let i = 0; i < src.length; i++ ) {
+        r.push(callback(src[i]))
+    }
+    return r
+}
+
+function reduce(src, callback, startingValue) {
+    let total;
+
+    if (startingValue) {
+        total = startingValue;
+        for (let i = 0; i < src.length; i++ ) {
+            total = callback(src[i], total);
+        }
+        return total
+    } else {
+        total = src[0];
+        for (let i = 1; i < src.length; i++ ) {
+            total = callback(src[i], total);
+        }
+        return total
+    }
+    
+}
